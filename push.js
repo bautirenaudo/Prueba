c597ca2c5d0a8b63f21af7c8381320f2fd287cb3
bautirenaudo <78118334+bautirenaudo@users.noreply.github.com>
2022-01-08 18:07:39 -0300

    Add files via upload

--- push.js
+++ push.js
@@ -7,6 +7,13 @@ let vapidKeys = {
 
 push.setVapidDetails("mailto:test@code.co.uk", vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub={};
+let sub = {
+    endpoint: "https://fcm.googleapis.com/fcm/send/ekjaf0BmqNE:APA91bH4aBSWIDwtKgUikgUTQGjjuODwp3Ta6XK1_UuHthpGglmQxft0w8siGQPx72tTo3Xy1GYPiVmsRfXf8pAfS1IxfHkvsMn3dE8DFiObbkNgoenpYG50nTdzcDx733lTWufDLz-W",
+    expirationTime: null,
+    keys: {
+        "p256dh": "BFUMGLTZOhbuYTmU7P8CnGofow0Iu-lEOgTkYi1tZwrQCoQhUg_DckBRLL6vgOkTzbzuo1R5-mteepHsoShzpjk",
+        auth: "xUY559B6DxJlchLNxiA4ZA"
+    }
+};
 
 push.sendNotification(sub, "test message");
